c02f4940a5ec30437a33dcd1d0eaf8b08665690d
WRyan <wesley.ryan03@gmail.com>
2024-09-06 14:40:49 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,21 +1,10 @@
 let  dados = [
-    {
-    titulo : "Carlos Sainz",
-    descricao : "Carlos Sainz, o piloto espanhol da Ferrari, tem se destacado no mundo da Fórmula 1 por sua habilidade, consistência e estilo de pilotagem elegante. Com uma carreira sólida e em constante evolução, Sainz tem conquistado o coração dos fãs com suas ultrapassagens precisas e sua capacidade de extrair o máximo de seus carros.",
-    link: "https://pt.wikipedia.org/wiki/Carlos_Sainz_Jr."
-    
-},
-{
-    titulo : "Charles Leclerc",
-    descricao : "Charles Leclerc, o jovem piloto monegasco da Ferrari, tem impressionado a Fórmula 1 com seu talento natural e velocidade. Com uma ascensão meteórica na categoria, Leclerc já conquistou diversas vitórias e se tornou um dos principais nomes do esporte.",
-    link: "https://pt.wikipedia.org/wiki/Charles_Leclerc"
-    
-},
-{
-    titulo : "Carlos Sainz",
-    descricao : "Max Verstappen, o piloto holandês da Red Bull Racing, é conhecido por sua agressividade e determinação nas pistas. Campeão mundial de Fórmula 1 em diversas ocasiões, Verstappen é considerado um dos maiores talentos da atual geração.",
-    link: "https://pt.wikipedia.org/wiki/Max_Verstappen"
-    
-}
+    "Rayssa Leal",
+    "Rebeca Andrade",
+    "Beatriz Souza"
 ];
 
+let rayssa = {
+    titulo : "Rayssa Leal"
+    descricao : 
+}
